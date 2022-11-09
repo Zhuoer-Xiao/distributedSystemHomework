5d9c16c4af0950c2c66ff6e1a278f9031ab3a22a
nightisyou <90315108+nightisyou@users.noreply.github.com>
2022-11-09 21:52:12 +0800

    Update client.go

--- client/client.go
+++ client/client.go
@@ -2,43 +2,63 @@ package client
 
 import (
 	"fmt"
-	log "github.com/Sirupsen/logrus"
 	"math/rand"
 
 	"GFS_Homework/common"
 )
 
-const (
-	READ   = 0x1
-	WRITE  = 0x2
-	RDWR   = 0x3
-	APPEND = 0x4
-	CREATE = 0x8
-)
-
-type FileMode uint32
-
-// 定义文件结构体
-type File struct {
-	fd int32 // filedata
-}
-
 type Client struct {
 	master common.ServerAddress
 }
 
-// 文件打开
-func OpenFile(filename string, flag int, perm FileMode) error {
+// 返回一个gfs client
+func NewClient(master common.ServerAddress) *Client {
+	return &Client{
+		master: master,
+	}
+}
 
+// 文件创建
+func (c *Client) Create(path common.Path) error {
+	var reply common.CreateFileReply
+	// master RPC 创建文件，给master一个路径，返回error
+	err := common.Call(string(c.master), "Master.RPCCreateFile", common.CreateFileArg{path}, &reply)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// 文件删除
+func (c *Client) Delete(path common.Path) error {
+	var reply common.DeleteFileReply
+	// master RPC 删除文件，给master一个路径，返回error
+	err := common.Call(string(c.master), "Master.RPCDeleteFile", common.DeleteFileArg{path}, &reply)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// 判断文件是否存在
+func (c *Client) IsExist(path common.Path) (ex bool, err error) {
+	var reply common.IsExistReply
+
+	err = common.Call(string(c.master), "Master.RPCGetFileInfo", common.IsExistArg{path}, &reply)
+	if err != nil {
+		return false, fmt.Errorf("File not exist")
+	}
+
+	return true, nil
 }
 
 // 文件读操作
 func (c *Client) Read(path common.Path, offset common.Offset, data []byte) (n int, err error) { // 内存分配data长度的空间
 	var file common.GetFileInfoReply
 	// master rpc
-	errx := common.Call(string(c.master), "Master.RPCGetFileInfo", common.GetFileInfoArg{path}, &file)
-	if errx != nil {
-		return -1, errx
+	err = common.Call(string(c.master), "Master.RPCGetFileInfo", common.GetFileInfoArg{path}, &file)
+	if err != nil {
+		return -1, err
 	}
 
 	// 判断读偏移量是否合法
@@ -63,7 +83,7 @@ func (c *Client) Read(path common.Path, offset common.Offset, data []byte) (n in
 			if err == nil {
 				break
 			}
-			log.Warning("Read ", handle, " connection error, please try again: ", err)
+			fmt.Errorf("Read ", handle, " connection error, please try again: ", err)
 		}
 
 		offset += common.Offset(n)
@@ -73,34 +93,72 @@ func (c *Client) Read(path common.Path, offset common.Offset, data []byte) (n in
 		}
 	}
 
+	if err != nil {
+		return pos, nil
+	} else {
+		return pos, err
+	}
 }
 
 // 文件写操作
-func (f *File) Write(content []byte) (int, error) {
+func (c *Client) Write(path common.Path, offset common.Offset, data []byte) error {
+	var file common.GetFileInfoReply
+	err := common.Call(string(c.master), "Master.RPCGetFileInfo", common.GetFileInfoArg{path}, &file)
+	if err != nil {
+		return err
+	}
 
-}
+	if int64(offset/common.MaxChunkSize) > file.Chunks {
+		return fmt.Errorf("Write offset exceeds the max file size")
+	}
 
-// 文件追加写操作
-func (f *File) Append() {
+	begin := 0
+	for {
+		index := common.ChunkIndex(offset / common.MaxChunkSize)
+		chunk_offset := offset % common.MaxChunkSize
 
-}
+		handle, err := c.GetChunkHandle(path, index)
+		if err != nil {
+			return err
+		}
 
-// 查询文件是否存在
-func (f *File) IsExist(filename string) (bool, error) {
+		writeMax := int(common.MaxChunkSize - chunk_offset)
+		var writeLen int
+		if begin+writeMax > len(data) { // 剩余空间够写入
+			writeLen = len(data) - begin
+		} else { // 否则只能写到chunk结尾
+			writeLen = writeMax
+		}
 
-}
+		for {
+			err = c.WriteChunk(handle, chunk_offset, data[begin:begin+writeLen])
+			if err == nil {
+				break
+			}
+			fmt.Errorf("Write ", handle, " connection error, please try again: ", err)
+		}
+		if err != nil {
+			return err
+		}
 
-// 文件删除操作
-func (f *File) Delete() {
-}
+		offset += common.Offset(writeLen) // 在总偏移量上记录已写的数据长度
+		begin += writeLen
 
-// 文件关闭
-func (f *File) Close() error {
+		if begin == len(data) {
+			break
+		}
+	}
+
 	return nil
 }
 
+// 文件追加写操作
+func (c *Client) Append() {
+
+}
+
 ////////////////////////////////////
-//具体功能实现
+//具体功能实现                      //
 ////////////////////////////////////
 
 // 寻找chunkhandle
@@ -116,6 +174,7 @@ func (c *Client) GetChunkHandle(path common.Path, index common.ChunkIndex) (comm
 }
 
 // 从给定offset开始阅读文件
+// 返回：读取数据长度，error
 func (c *Client) ReadChunk(handle common.ChunkHandle, offset common.Offset, data []byte) (int, error) {
 	var readLen int // 欲读数据的长度
 
@@ -146,4 +205,42 @@ func (c *Client) ReadChunk(handle common.ChunkHandle, offset common.Offset, data
 	}
 
 	return r.Length, nil
+}
+
+// 从给定offset开始写入文件
+func (c *Client) WriteChunk(handle common.ChunkHandle, offset common.Offset, data []byte) error {
+	if len(data)+int(offset) > common.MaxChunkSize {
+		return fmt.Errorf("Current data lengths + Current offset exceeds the max chunk size")
+	}
+
+	var l common.GetReplicasReply
+	err := common.Call(string(c.master), "Master.RPCGetReplicas", common.GetReplicasArg{handle}, &l)
+	if err != nil {
+		return common.Error{common.UnknownError, err.Error()}
+	}
+
+	// 不考虑租约，依次写入所有副本
+	current := 0
+	for {
+		location := l.Locations[current]
+
+		if len(l.Locations) == 0 {
+			return common.Error{common.UnknownError, "No replica found"}
+		}
+
+		// chunkserver rpc
+		var r common.WriteChunkReply
+		wcargs := common.WriteChunkArg{handle, offset, data}
+		err = common.Call(string(location), "ChunkServer.RPCWriteChunk", wcargs, &r)
+		if err != nil {
+			return common.Error{common.UnknownError, err.Error()}
+		}
+
+		current += 1
+		if current == len(l.Locations) {
+			break
+		}
+	}
+
+	return nil
 }
